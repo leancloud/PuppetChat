b8ba841d2b7592ca0c73885ee912f8f607bb0a6c
Tianyong Tang <tang3w@gmail.com>
2017-03-15 16:00:15 +0800

    Customize class for table view cell of conversation member

---
+++ PuppetChat/PCClientTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  PCClientTableViewCell.m
+//  PuppetChat
+//
+//  Created by Tang Tianyong on 3/15/17.
+//  Copyright © 2017 Tianyong Tang. All rights reserved.
+//
+
+#import "PCClientTableViewCell.h"
+
+@implementation PCClientTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
