f76496d83d29085ac136fdb52e776e807daa5e9c
Tianyong Tang <tang3w@gmail.com>
2017-03-15 16:17:42 +0800

    Customize class of client view controller

---
+++ PuppetChat/PCClientTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCClientTableViewController.h
+//  PuppetChat
+//
+//  Created by Tang Tianyong on 3/15/17.
+//  Copyright © 2017 Tianyong Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCClientTableViewController : UITableViewController
+
+@end
---
+++ PuppetChat/PCClientTableViewController.m
@@ -0,0 +1,21 @@
+//
+//  PCClientTableViewController.m
+//  PuppetChat
+//
+//  Created by Tang Tianyong on 3/15/17.
+//  Copyright © 2017 Tianyong Tang. All rights reserved.
+//
+
+#import "PCClientTableViewController.h"
+
+@interface PCClientTableViewController ()
+
+@end
+
+@implementation PCClientTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
