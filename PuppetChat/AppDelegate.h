8e9775c6054e8aac56f31658d945feef66e6ff9e
Tianyong Tang <tang3w@gmail.com>
2017-03-15 11:20:22 +0800

    Init project

---
+++ PuppetChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PuppetChat
+//
+//  Created by Tang Tianyong on 3/15/17.
+//  Copyright © 2017 Tianyong Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PuppetChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PuppetChat
+//
+//  Created by Tang Tianyong on 3/15/17.
+//  Copyright © 2017 Tianyong Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
