e76d0a8433764d4695c9e57b1ce6279699bb3e73
Tianyong Tang <tang3w@gmail.com>
2017-03-16 11:37:24 +0800

    Add puppet class

---
+++ PuppetChat/PCPuppet.h
@@ -0,0 +1,13 @@
+//
+//  PCPuppet.h
+//  PuppetChat
+//
+//  Created by Tang Tianyong on 3/16/17.
+//  Copyright © 2017 Tianyong Tang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PCPuppet : NSObject
+
+@end
---
+++ PuppetChat/PCPuppet.m
@@ -0,0 +1,13 @@
+//
+//  PCPuppet.m
+//  PuppetChat
+//
+//  Created by Tang Tianyong on 3/16/17.
+//  Copyright © 2017 Tianyong Tang. All rights reserved.
+//
+
+#import "PCPuppet.h"
+
+@implementation PCPuppet
+
+@end
